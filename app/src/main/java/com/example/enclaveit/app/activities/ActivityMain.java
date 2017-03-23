4b4fe26916d7201e75dfd0477aae5f7f319fa3ec
danisluis6 <vuongluis@hotmail.com>
2017-03-23 10:40:49 +0700

    version3

--- app/src/main/java/com/example/enclaveit/app/activities/ActivityMain.java
+++ app/src/main/java/com/example/enclaveit/app/activities/ActivityMain.java
@@ -1,18 +1,28 @@
 package com.example.enclaveit.app.activities;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.enclaveit.app.R;
+import com.example.enclaveit.app.config.Config;
+import com.example.enclaveit.app.utils.NotificationUtils;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class ActivityMain extends AppCompatActivity {
 
     private static final String TAG = ActivityMain.class.getSimpleName();
-    private TextView txtRegId;
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+    private TextView txtRegId, txtMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +30,24 @@ public class ActivityMain extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txtRegId = (TextView) findViewById(R.id.txt_reg_id);
+        txtMessage = (TextView) findViewById(R.id.txt_push_message);
 
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                // checking for type intent filter
+                if (intent.getAction().equals(Config.REGISTRATION_COMPLETE)) {
+                    displayFirebaseRegId();
+                } else if (intent.getAction().equals(Config.PUSH_NOTIFICATION)) {
+                    String message = intent.getStringExtra("message");
+                    Toast.makeText(getApplicationContext(), "Push notification: " + message, Toast.LENGTH_LONG).show();
+                    txtMessage.setText(message);
+                }else{
+                    Toast.makeText(ActivityMain.this, "BroadcastReceiver is null", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
         displayFirebaseRegId();
     }
 
@@ -31,6 +58,29 @@ public class ActivityMain extends AppCompatActivity {
             txtRegId.setText("Firebase Reg Id: " + regId);
         else
             txtRegId.setText("Firebase Reg Id is not received yet!");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // register GCM registration complete receiver
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(Config.REGISTRATION_COMPLETE));
+
+        // register new push message receiver
+        // by doing this, the activity will be notified each time a new message arrives
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(Config.PUSH_NOTIFICATION));
+
+        // clear the notification area when the app is opened
+        NotificationUtils.clearNotifications(getApplicationContext());
+    }
+
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
     }
 }
 
